390c697c8b1549965c13ba0eafbf9432dbf8db89
Ashutosh Lembhe <ashutoshlembhe50@gmail.com>
2020-09-17 01:27:37 +0530

    Add files via upload

---
+++ sketch_feb25b/sketch_feb25b.ino
@@ -0,0 +1,26 @@
+int SensorPin= 1;
+int raw=0;
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  raw=analogRead(SensorPin);
+  float mv=(raw/1024.0)*5000;
+  float cel=mv/10;
+  float far=(cel*9)/5+32;
+  Serial.print(" Temperature=");
+  Serial.print(cel);
+  Serial.print("*C");
+  delay(1000);
+  Serial.print("\n Tempearture=");
+  Serial.print(far);
+  Serial.print("*F ");
+  
+  
+}
